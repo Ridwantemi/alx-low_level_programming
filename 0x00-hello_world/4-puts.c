c7f292768458e1f3f26c5821fff14dd5b6f5e5f0
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-08 10:09:56 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #!/bin/bash
+#include <stdio.h>
+
 /**
  * main - prints a string
- * Description: prints "Programming is like building a multilingual puzzle"
- * Return: 0  
+ * Description: prints "Programming is like building a multilingual puzzle
+ * *Return: 0
  */
-int main(void)
-{
-	puts("Programming is like bulding a multilingual puzzle\n");
-	return (0);
-}
 
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+
+
+}
