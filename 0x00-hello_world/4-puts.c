c0ca9cd2b7f603d9aef0aa7eaa9407860d593f0a
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-08 13:44:20 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 
-/**
+ /**
  * main - prints a string
  * Description: prints "Programming is like building a multilingual puzzle
  * *Return: 0
- */
+ * /
 
 int main(void)
 
