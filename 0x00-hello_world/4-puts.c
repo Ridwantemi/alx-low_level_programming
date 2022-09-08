84053f2d9e7ac032af870175bfe4a221e3097853
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-08 09:51:28 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success) 
+ */
+int main(void)
+{
+	printf("Programming is like bulding a multilingual puzzle\n");
+	return (0);
+}
+
