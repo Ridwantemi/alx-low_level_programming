613771763394acaa9688dea2260b99b4c1498a50
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-13 12:53:30 -0700

    main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- *print_alphabet - Entry point
- *Description: a function that prints the alphabet,
-in lowercase followed by a new line
- *Return: void
+ *print_alphabet - function to print abcd
+ *Return: Always 0
  */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
+char alp = 'a';
+while (alp <= 'z')
 {
-_putchar(letter);
-letter++;
+_putchar (alp);
+alp++;
 }
-_putchar('\n');
-return (0);
+_putchar ('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,19 @@
-1   int _putchar(char c);
-2   void print_alphabet(void);
-3   void print_alphabet_x10(void);
-4   int _islower(int c);
-5   int _isalpha(int c);
-6   int print_sign(int n);
-7   int _abs(int);
-8   int print_last_digit(int);
-9   void jack_bauer(void);
-10  void times_table(void);
-11  int add(int, int);
-12  void print_to_98(int n);
-13  void print_times_table(int n);
-
+1   #ifndef HOLBERTON_H_INCLUDED
+2   #define HOLBERTON_H_INCLUDED
+3
+4   int _putchar(char c);
+5   void print_alphabet(void);
+6   void print_alphabet_x10(void);
+7   int _islower(int c);
+8   int _isalpha(int c);
+9   int print_sign(int n);
+10  int _abs(int);
+11  int print_last_digit(int);
+12  void jack_bauer(void);
+13  void times_table(void);
+14  int add(int, int);
+15  void print_to_98(int n);
+16  void print_times_table(int n);
+17  void num(int r, int n);
+18
+19  #endif
