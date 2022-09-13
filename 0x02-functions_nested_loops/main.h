fa39a581958842a8ac0995b14a6a725290165c6b
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-13 13:02:41 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,19 @@
 1   #ifndef HOLBERTON_H_INCLUDED
 2   #define HOLBERTON_H_INCLUDED
 3
-4   int _putchar(char c);
-5   void print_alphabet(void);
-6   void print_alphabet_x10(void);
-7   int _islower(int c);
-8   int _isalpha(int c);
-9   int print_sign(int n);
-10  int _abs(int);
-11  int print_last_digit(int);
-12  void jack_bauer(void);
-13  void times_table(void);
-14  int add(int, int);
-15  void print_to_98(int n);
-16  void print_times_table(int n);
-17  void num(int r, int n);
+4   (int _putchar(char c));
+5   (void print_alphabet(void));
+6   (void print_alphabet_x10(void));
+7   (int _islower(int c));
+8   (int _isalpha(int c));
+9   (int print_sign(int n));
+10  (int _abs(int));
+11  (int print_last_digit(int));
+12  (void jack_bauer(void));
+13  (void times_table(void));
+14  (int add(int, int));
+15  (void print_to_98(int n));
+16  (void print_times_tab0e(int n));
+17  (void num(int r, int n));
 18
-19  #endif
+19  #(endif)
