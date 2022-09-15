6d3f39530e7a99567716146f52565c3d925ee509
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-15 15:41:46 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@
  *main - tests function that prints if integer is positive or negative
  *Return: 0
  */
-
 int main(void)
 {
 int i;
@@ -12,4 +11,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-
