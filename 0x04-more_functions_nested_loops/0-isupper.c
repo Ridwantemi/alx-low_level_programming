834c388420c63b686b856148432013115ead5708
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-15 12:00:09 -0700

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *_isupper - to print only uppercase
- *@c - character to check for
+ *@c: character to check for
  *Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 65 && c <= 90)
 return (1);
 else
 return (0);
