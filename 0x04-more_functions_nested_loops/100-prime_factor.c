984cb998c9c00d155a08c78ed0555a9778c91f0b
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-15 13:47:36 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main -  calculate largest prime of 612852475143
+ *Return: Void
+ */
+int main(void)
+{
+long int x = 612852475143;
+long int py;
+for (py = 2; py < x; py++)
+{
+if (x % py == 0)
+{
+x = x / py;
+}
+}
+printf("%ld\n", py);
+return (0);
+}
+
+
