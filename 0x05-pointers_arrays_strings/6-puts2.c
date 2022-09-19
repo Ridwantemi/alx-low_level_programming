24209cce2cbd0e1df9e26476171f539104f4a806
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-19 14:51:14 -0700

    pointer

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,29 @@
 #include "main.h"
 /**
- *puts2 - prints one char out of 2
- *@str: type
- *Return: 0
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: length
  */
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != '\0')
+{
+l++;
+s++;
+}
+return (l);
+}
+
+/**
+ *puts2 - prints every second character of a string, followed by a new line
+ *@str: string to print
+ */
+
 void puts2(char *str)
 {
-int i;
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
-{
-_putchar(str[i]);
-}
-i++;
-}
+int b;
+for (b = 0; str[b] != '\0' && b < _strlen(str); b += 2)
+_putchar(str[b]);
 _putchar('\n');
 }
