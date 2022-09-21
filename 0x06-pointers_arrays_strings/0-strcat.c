a6ada1749ea780148e2c5a6a6efbc945444511a6
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-21 16:59:22 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ **_strcat - concatenates two strings
+ *@dest: dest
+ *@src: src
+ *Return: string
+ */
+char *_strcat(char *dest, char *src);
+{
+char *dest[] = "dest";
+char *src[] = "src";
+_strcat(char *dest, char *src);
+_putchar("result: %s\n", *dest);
+}
