bc8f3cbf796aa7608b1c20e315347e97e88a0131
Ridwantemi <obayoporidwantemi@gmail.com>
2022-09-22 01:57:49 -0700

    main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,22 @@ char *_strcat(char *dest, char *src);
 {
 char *dest[] = "dest";
 char *src[] = "src";
-_strcat(char *dest, char *src);
-_putchar("result: %s\n", *dest);
+char dest;
+
+int i;
+int j;
+
+while (dest[i] != '\0')
+{
+i++;
 }
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
