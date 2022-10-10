490628b2da02c9f9d5b632fd517283438718ddbc
Ridwantemi <obayoporidwantemi@gmail.com>
2022-10-10 12:27:40 -0700

    typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,16 +4,16 @@
 /**
  *init_dog - function
  *struct dog - struct type
- *@*d - memory address
- *@name - name
- *@age - dog age
- *@owner - dog owner
+ *@d: memory address
+ *@name: name
+ *@age: dog age
+ *@owner: dog owner
  *Return - 0 on success
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 	(*d).name = name;
 	(*d).age = age;
